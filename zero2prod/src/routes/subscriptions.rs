2719fd57a6bbdf4b3df48ed9ddcf0686cf6810e6
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-02-15 13:32:08 -0500

    Validation, AsRef, and Result Enum for error validation

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -1,3 +1,4 @@
+use crate::domain::{NewSubscriber, SubscriberName};
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
@@ -11,31 +12,39 @@ pub struct FormData {
 }
 #[tracing::instrument(name="Adding a new subscriber", 
 skip(form, pool),
-fields(request_id = %Uuid::new_v4(),email=%form.email,name=%form.name))]
+fields(email=%form.email,name=%form.name))]
 
 pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
-    insert_subscriber(&pool, &form)
+    let name = SubscriberName::parse(form.0.name).map_err(|e| HttpResponse::BadRequest().finish())?;
+    let new_subscriber = NewSubscriber{
+        email:form.0.email,
+        name,
+    };
+    insert_subscriber(&pool, &new_subscriber)
         .await
-        .map_err(|e| HttpResponse::InternalServerError().finish())?;
+        .map_err(|_| HttpResponse::InternalServerError().finish())?;
     Ok(HttpResponse::Ok().finish())
 }
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(form, pool)
+    skip(new_subscriber, pool)
 )]
 
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    new_subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email,
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
